b4408b68beab19fb72e7eec6fbe4c755e936f446
brTomaz <bruno.tomaz@aluno.ufop.edu.br>
2020-12-17 09:00:27 -0300

    fix: change environment variables

--- src/services/api/base/http-client.ts
+++ src/services/api/base/http-client.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const REVISE_BASE_URL = `${process.env.API_HOST}:${process.env.API_PORT}`
+const REVISE_BASE_URL = `${process.env.API_URL}`
 
 export const httpClient = axios.create({
   baseURL: REVISE_BASE_URL
