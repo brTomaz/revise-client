a26709b9be3c8818f633de02b8c47ad945d7725a
brTomaz <bruno.tomaz@aluno.ufop.edu.br>
2020-12-29 18:22:01 -0300

    feat: add Quicksand as the main font

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -4,10 +4,39 @@ import 'react-toastify/dist/ReactToastify.min.css'
 import theme from './theme'
 
 const GlobalStyles = createGlobalStyle`
+  @font-face {
+    font-family: 'Quicksand';
+    font-style: normal;
+    font-weight: 300;
+    font-display: swap;
+    src: local('Quicksand Light'), local('Quicksand-Light'),
+        url('/fonts/Quicksand-Light.woff2') format('woff2');
+  }
+
+  @font-face {
+    font-family: 'Quicksand';
+    font-style: normal;
+    font-weight: 400;
+    font-display: swap;
+    src: local('Quicksand Light'), local('Quicksand-Light'),
+        url('/fonts/Quicksand-Regular.woff2') format('woff2');
+  }
+
+  @font-face {
+    font-family: 'Quicksand';
+    font-style: normal;
+    font-weight: 600;
+    font-display: swap;
+    src: local('Quicksand Bold'), local('Quicksand-Bold'),
+        url('/fonts/Quicksand-Bold.woff2') format('woff2');
+  }
+
   * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
   }
 
   html {
@@ -16,8 +45,7 @@ const GlobalStyles = createGlobalStyle`
 
   html, body, #__next {
     height: 100%;
-    background-color: ${theme.colors.black};
-    background-image: url('https://www.transparenttextures.com/patterns/inspiration-geometry.png');
+    background-color: ${theme.colors.mainBg};
   }
 
   body {
