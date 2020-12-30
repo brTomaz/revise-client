b4ee575faf20caa74f7d6dea1e3f8a0aebaac4fa
brTomaz <bruno.tomaz@aluno.ufop.edu.br>
2020-12-30 00:59:41 -0300

    refactor: change Toast styles

--- src/components/Toast/styles.ts
+++ src/components/Toast/styles.ts
@@ -1,27 +1,29 @@
 import styled, { css, DefaultTheme } from 'styled-components'
 import { ToastContainer } from 'react-toastify'
+import media from 'styled-media-query'
 
 export const BaseToast = (theme: DefaultTheme) => css`
   background-color: ${theme.colors.primary};
   border-radius: 0.5rem;
   color: ${theme.colors.white};
-  padding-bottom: ${theme.spacings.small};
+  font-family: inherit;
+`
 
-  div {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-
-    svg {
-      width: 4rem;
-    }
-  }
+export const ToastSuccessWrapper = styled.div`
+  display: flex;
 `
 
 export const Wrapper = styled(ToastContainer)`
+  width: 100%;
+
+  ${media.greaterThan('medium')`
+    width: 38rem;
+  `}
+
   .Toastify__toast--success {
     ${({ theme }) => css`
       ${BaseToast(theme)};
+
       svg {
         color: ${theme.colors.white};
       }
@@ -29,22 +31,26 @@ export const Wrapper = styled(ToastContainer)`
   }
 `
 
-export const Points = styled.div`
+export const Points = styled.h3`
   ${({ theme }) => css`
-    background-color: ${theme.colors.white};
-    border-radius: 7px;
-    font-size: ${theme.font.sizes.xhuge};
+    font-size: ${theme.font.sizes.xlarge};
     font-weight: ${theme.font.bold};
-    text-align: center;
-    color: ${theme.colors.primary};
-    padding: ${theme.spacings.xsmall};
-    margin: ${theme.spacings.xsmall} 0;
+    color: ${theme.colors.white};
+    margin-bottom: ${theme.spacings.xxsmall};
   `}
 `
 export const Attempt = styled.p`
   ${({ theme }) => css`
-    color: ${theme.colors.white};
+    color: ${theme.colors.gray};
     font-size: ${theme.font.sizes.medium};
-    font-weight: ${theme.font.bold};
+  `}
+`
+
+export const TextInfoWrapper = styled.div`
+  ${({ theme }) => css`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    margin: 0 ${theme.spacings.small};
   `}
 `
